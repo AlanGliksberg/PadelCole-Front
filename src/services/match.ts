23441475c25348fa46ae5f07436a6fa70d58f7c7
Alan Gliksberg <alan.glik@gmail.com>
2025-06-03 21:15:51 -0300

    agrego parametro de cache a llamado de partidos

--- src/services/match.ts
+++ src/services/match.ts
@@ -1,9 +1,13 @@
 import { GET_MY_MATCHES_URI } from "../constants/api";
 import { get } from "./api";
 
-export const getCreatedMatches = async <T>(page: number, pageSize: number) => {
+export const getCreatedMatches = async <T>(
+  page: number,
+  pageSize: number,
+  withCache = true
+) => {
   return await get<T>(GET_MY_MATCHES_URI, {
     queryParams: { page, pageSize, createdBy: true },
-    withCache: true,
+    withCache,
   });
 };
