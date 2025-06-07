12ce0739d1696c95bf88c89e356d2769fe997300
AlanGliksberg <alan.gliksberg@globons.com>
2025-06-07 12:45:05 -0300

    ajuste modal

--- src/components/Modals/PlayerDetailsModal.tsx
+++ src/components/Modals/PlayerDetailsModal.tsx
@@ -2,7 +2,12 @@ import { colors } from "@/src/theme";
 import { Player } from "@/src/types";
 import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
-import { Modal, TouchableOpacity, View } from "react-native";
+import {
+  Modal,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import PlayerAvatar from "../PlayerAvatar/PlayerAvatar";
 import CustomText from "../ui/CustomText/CustomText";
 import { styles } from "./PlayerDetailsModal.styles";
@@ -23,59 +28,66 @@ const PlayerDetailsModal: React.FC<PlayerDetailsModalProps> = ({
       transparent={true}
       onRequestClose={closePlayerDetail}
     >
-      <TouchableOpacity style={styles.backdrop} onPress={closePlayerDetail}>
-        <TouchableOpacity style={styles.modalContainer} activeOpacity={1}>
-          <View style={styles.header}>
-            <View style={styles.nameContainer}>
-              <PlayerAvatar player={player!} size="m" inverse touchable={false} />
-              <CustomText style={styles.name}>
-                {`${player?.firstName} ${player?.lastName}`}
-              </CustomText>
+      <TouchableWithoutFeedback onPress={closePlayerDetail}>
+        <View style={styles.backdrop}>
+          <TouchableOpacity style={styles.modalContainer} activeOpacity={1}>
+            <View style={styles.header}>
+              <View style={styles.nameContainer}>
+                <PlayerAvatar
+                  player={player!}
+                  size="m"
+                  inverse
+                  touchable={false}
+                />
+                <CustomText style={styles.name}>
+                  {`${player?.firstName} ${player?.lastName}`}
+                </CustomText>
+              </View>
+              <View>
+                <TouchableOpacity onPress={closePlayerDetail}>
+                  <MaterialIcons name="close" size={28} color={colors.white} />
+                </TouchableOpacity>
+              </View>
             </View>
-            <View>
-              <TouchableOpacity onPress={closePlayerDetail}>
-                <MaterialIcons name="close" size={28} color={colors.white} />
-              </TouchableOpacity>
+            <View style={styles.row}>
+              <View style={styles.rowItem}>
+                <CustomText type="medium" bold>
+                  Género:{" "}
+                </CustomText>
+                <CustomText type="small">
+                  {player?.gender?.name || "No informado"}
+                </CustomText>
+              </View>
+              <View style={styles.rowItem}>
+                <CustomText type="medium" bold>
+                  Posición:{" "}
+                </CustomText>
+                <CustomText type="small">
+                  {player?.position?.description || "No informado"}
+                </CustomText>
+              </View>
             </View>
-          </View>
-          <View style={styles.row}>
-            <View style={styles.rowItem}>
-              <CustomText type="medium" bold>
-                Género:{" "}
-              </CustomText>
-              <CustomText type="small">
-                {player?.gender?.name || "No informado"}
-              </CustomText>
+            <View style={styles.row}>
+              <View style={styles.rowItem}>
+                <CustomText type="medium" bold>
+                  Categoría:{" "}
+                </CustomText>
+                <CustomText type="small">
+                  {player?.category?.description || "No informado"}
+                </CustomText>
+              </View>
+              <View style={styles.rowItem}>
+                <CustomText type="medium" bold>
+                  Teléfono:{" "}
+                </CustomText>
+                <CustomText type="small">
+                  {player?.phone || "No informado"}
+                </CustomText>
+              </View>
             </View>
-            <View style={styles.rowItem}>
-              <CustomText type="medium" bold>
-                Posición:{" "}
-              </CustomText>
-              <CustomText type="small">
-                {player?.position?.description || "No informado"}
-              </CustomText>
-            </View>
-          </View>
-          <View style={styles.row}>
-            <View style={styles.rowItem}>
-              <CustomText type="medium" bold>
-                Categoría:{" "}
-              </CustomText>
-              <CustomText type="small">
-                {player?.category?.description || "No informado"}
-              </CustomText>
-            </View>
-            <View style={styles.rowItem}>
-              <CustomText type="medium" bold>
-                Teléfono:{" "}
-              </CustomText>
-              <CustomText type="small">
-                {player?.phone || "No informado"}
-              </CustomText>
-            </View>
-          </View>
-        </TouchableOpacity>
-      </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
     </Modal>
   );
 };
