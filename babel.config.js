8c6e95c16974588e0559ea6953f0b2e98e66efa4
AlanGliksberg <alan.gliksberg@globons.com>
2025-10-15 11:20:08 -0300

    configuraciones

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
-  api.cache(true)
+module.exports = function (api) {
+  api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-  }
-}
+    presets: ["babel-preset-expo"],
+    plugins: [
+      // Habilita alias @ y @src para Metro/Babel
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./",
+            "@src": "./src",
+          },
+          extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+        },
+      ],
+      "expo-router/babel",
+      "react-native-reanimated/plugin",
+    ],
+  };
+};
